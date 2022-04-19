91789a9b714fb129b042db126c28bdd644def615
dan birle <teodorabirle.ppb@gmail.com>
2022-04-19 17:19:21 +0300

    Improvement with a negative scenario

--- steps/heroku_login_steps.py
+++ steps/heroku_login_steps.py
@@ -8,14 +8,26 @@ from behave import *
 def step_impl(context):
     context.heroku_login_page.navigate_to_login_page()
 
-@when('heroku_login_page: I fill tomsmith in the username field')
-def step_impl(context):
-    context.heroku_login_page.fill_username()
+@when('heroku_login_page: I fill {username} in the username field')
+def step_impl(context, username):
+    context.heroku_login_page.fill_username(username)
 
-@when('heroku_login_page: I fill SuperSecretPassword! in the password field')
-def step_impl(context):
-    context.heroku_login_page.fill_password()
+@when('heroku_login_page: I fill {password} in the password field')
+def step_impl(context, password):
+    context.heroku_login_page.fill_password(password)
 
 @when('heroku_login_page: I click on the Login button')
 def step_impl(context):
     context.heroku_login_page.click_login_btn()
+
+@then('heroku_login_page: I see the error message displayed')
+def step_impl(context):
+    context.heroku_login_page.see_error_message()
+
+@when('heroku_login_page: I click the X on the error message banner')
+def step_impl(context):
+    context.heroku_login_page.click_error_message()
+
+@then('heroku_login_page: I not see the error message displayed anymore')
+def step_impl(context):
+    context.heroku_login_page.see_error_message()
